ba948a12645e233a1635df2f6827ac87e8a8cde5
lUKAWESTFALDi <westfaldluka49@gmail.com>
2024-10-19 18:02:45 +0400

    first commit

---
+++ day 00/level 3/cw.py
@@ -0,0 +1,12 @@
+name = 'luka'
+last_name = 'westfald'
+print("luka  westfald")
+country = 'georgia'
+favorite_car = 'G wagon'
+hobby = 'Football'
+favorite_color = 'Red'
+print("georgia")
+print("G wagon")
+print("Football")
+print("Red")
+print("")
